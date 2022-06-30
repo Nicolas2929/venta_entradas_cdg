64f1ffd9ef3b8f4d07c0c25873700536bd42f215
Nicolas2929 <108443720+Nicolas2929@users.noreply.github.com>
2022-06-30 09:22:02 -0400

    complete todo el server

---
+++ server/src/components/orden_pago.java
@@ -0,0 +1,69 @@
+package components;
+
+import java.sql.SQLException;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import Servisofts.SPGConect;
+import Servisofts.SUtil;
+
+public class orden_pago {
+    public static final String COMPONENT = "orden_pago";
+
+    public static void onMessage(JSONObject obj) {
+        try {
+            switch (obj.getString("type")) {
+                case "registro":
+                    registro(obj);
+                    break;
+                case "editar":
+                    editar(obj);
+                    break;
+                case "getAll":
+                    getAll(obj);
+                    break;
+                case "getByKey":
+                    getByKey(obj);
+                    break;
+            }
+        } catch (Exception e) {
+            obj.put("estado", "error");
+            obj.put("error", e.getMessage());
+        }
+    }
+
+    public static void registro(JSONObject obj) throws SQLException {
+        JSONObject data = obj.getJSONObject("data");
+        data.put("key", SUtil.uuid());
+        data.put("fecha_on", SUtil.now());
+        data.put("estado", 1);
+        SPGConect.insertObject(COMPONENT, data);
+        obj.put("data", data);
+        obj.put("estado", "exito");
+    }
+
+    public static void editar(JSONObject obj) throws SQLException {
+        JSONObject data = obj.getJSONObject("data");
+        SPGConect.editObject(COMPONENT, data);
+        obj.put("data", data);
+        obj.put("estado", "exito");
+    }
+
+    public static void getAll(JSONObject obj) throws SQLException {
+        JSONArray arr = SPGConect
+                .ejecutarConsultaArray(
+                        "SELECT array_to_json(array_agg(" + COMPONENT + ".*)) as json FROM " + COMPONENT + "");
+        obj.put("data", arr);
+        obj.put("estado", "exito");
+    }
+
+    public static void getByKey(JSONObject obj) throws SQLException {
+        JSONObject arr = SPGConect
+                .ejecutarConsultaObject(
+                        "SELECT to_json(" + COMPONENT + ".*) as json FROM " + COMPONENT + " WHERE key = '"
+                                + obj.getString("key") + "'");
+        obj.put("data", arr);
+        obj.put("estado", "exito");
+    }
+}
